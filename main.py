401af7915d6b81e3f64f517d2cf17c8f30ab72e0
Andreas Albrecht <a.albrecht25@gmx.de>
2019-01-06 20:35:31 +0100

    Creating a function to initialize a random matrix of desired shape

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ import numpy as np
 from math import pi
 import help_functions
 
+# a simple function to create a randomly initialized matrix of desired shape (nx, ny)
+def initialize(nx, ny):
+    mat = np.random.randn(nx, ny)
+    return mat
 
-def main():
-    mat = np.random.randn(4,3)
+
+'''
+    a main function that does not do much yet
+'''
+def main(mat):
+
     av_vec = help_functions.calc_average(mat)
 
     print("Dimension of av_vec: " + str(av_vec.shape) )
 
 
-main()    
+mat = initialize(4,3)
+main(mat)    
 
